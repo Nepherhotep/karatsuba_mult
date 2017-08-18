1f4be3f2ccaf63f8fde0d683afcc943cc856eb35
Alexey Zankevich <alex.zankevich@gmail.com>
2017-08-18 22:59:21 +0300

    Implemented simple mult algorithm

--- karatsuba_mult.py
+++ karatsuba_mult.py
@@ -32,6 +32,30 @@ def mult(a, b):
     return a * b
 
 
+def inc_value(arr, pos, value):
+    if value:
+        value += arr[pos]
+
+        mod = int(value / 10)
+        rem = value % 10
+
+        arr[pos] = rem
+        inc_value(arr, pos + 1, mod)
+
+
+def simple_mult(a, b, size=64):
+    a_arr, b_arr = int_to_array(a, size), int_to_array(b, size)
+    result = numpy.zeros(len(a_arr) + len(b_arr), dtype=int)
+    for a_pos, a_item in enumerate(a_arr):
+        for b_pos, b_item in enumerate(b_arr):
+            pos = a_pos + b_pos
+            value = mult(a_item, b_item)
+
+            inc_value(result, pos, value)
+
+    return array_to_int(result)
+
+
 def karatsuba_mult(value1, from1, to1, value2, from2, to2):
     if from1 - to1 == 2:
         a = value1[from1]
