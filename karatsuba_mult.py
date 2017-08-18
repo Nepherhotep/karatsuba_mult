a4a490071df4320623245d7bea192e331223e530
Alexey Zankevich <alexey@gettipsi.com>
2017-08-18 18:56:51 +0300

    Added array_to_int method

--- karatsuba_mult.py
+++ karatsuba_mult.py
@@ -6,7 +6,7 @@ def int_to_array(value, size=64):
     Convert integer into little-endian array
     :param value: integer
     :param size: length of array (should be power of 2)
-    :return:
+    :return: array
     """
     arr = numpy.zeros(size, dtype=int)
     for i, c in enumerate(str(value)[::-1]):
@@ -14,10 +14,30 @@ def int_to_array(value, size=64):
     return arr
 
 
+def array_to_int(value):
+    """
+    Convert little-endian array into integer
+    :param value: array
+    :return: int
+    """
+    result = ''.join(map(str, value))
+    result = result[::-1]
+    result = result.lstrip('0')
+    return int(result)
+
+
 def mult(a, b):
     assert a < 10
     assert b < 10
     return a * b
+
+
+def karatsuba_mult(value1, from1, to1, value2, from2, to2):
+    if from1 - to1 == 2:
+        a = value1[from1]
+        b = value1[to1]
+        c = value2[from1]
+        d = value2[to1]
 
 
 def main():
--- test_karatsuba_mult.py
+++ test_karatsuba_mult.py
@@ -2,16 +2,26 @@ from unittest import TestCase
 
 import numpy as np
 
-from karatsuba_mult import int_to_array
+from karatsuba_mult import int_to_array, array_to_int
 
 
 class IntToArrayTest(TestCase):
-    def test_completing_with_zeros(self):
+    def test_int_to_array_completing_with_zeros(self):
         value = int_to_array(1234, size=8)
 
         assert np.array_equal(value, np.array([4, 3, 2, 1, 0, 0, 0, 0]))
 
-    def test_full_size(self):
+    def test_int_to_array_full_size(self):
         value = int_to_array(1234, size=4)
 
         assert np.array_equal(value, np.array([4, 3, 2, 1]))
+
+    def test_array_to_int(self):
+        value = array_to_int(np.array([4, 3, 2, 1]))
+
+        assert value == 1234, value
+
+    def test_array_to_int_with_zeros(self):
+        value = array_to_int(np.array([0, 4, 3, 2, 1, 0, 0, 0, 0]))
+
+        assert value == 12340, value
