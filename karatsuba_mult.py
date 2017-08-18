e8550bb0dfaa12d39cf81994ca67769f3537a8bc
Alexey Zankevich <alexey@gettipsi.com>
2017-08-18 18:38:27 +0300

    Added comment to int_to_array function

--- karatsuba_mult.py
+++ karatsuba_mult.py
@@ -2,10 +2,22 @@ import numpy
 
 
 def int_to_array(value, size=64):
+    """
+    Convert integer into little-endian array
+    :param value: integer
+    :param size: length of array (should be power of 2)
+    :return:
+    """
     arr = numpy.zeros(size, dtype=int)
     for i, c in enumerate(str(value)[::-1]):
         arr[i] = int(c)
     return arr
+
+
+def mult(a, b):
+    assert a < 10
+    assert b < 10
+    return a * b
 
 
 def main():
