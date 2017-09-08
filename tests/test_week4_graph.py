488be0717bf23edb2a3817f54c14abff95299d70
Alexey Zankevich <alexey@gettipsi.com>
2017-09-08 18:47:19 +0300

    Added another test

---
+++ tests/test_week4_graph.py
@@ -0,0 +1,64 @@
+from unittest import TestCase
+
+from week4_graph import Graph
+
+
+class TestGraph(TestCase):
+    def test_replace_edge(self):
+        """
+        We use graph below
+
+        <1> --- <2>
+         |       |
+        <4> --- <3>
+
+        We want to replace 2 with 3:
+
+           --------+
+        <1> -----+ |
+         |       | |
+        <4> ---- <3>
+        """
+        g = Graph({1: [2, 4],
+                   2: [1, 3],
+                   3: [2, 4],
+                   4: [1, 3]})
+
+        g.replace_edge(2, 3)
+        g.sort()
+
+        expected = {1: [3, 3, 4],
+                    3: [1, 1, 4],
+                    4: [1, 3]}
+
+        self.assertEqual(g.graph, expected)
+
+
+    def replace_multiple_nodes(self):
+        """
+        We use graph below
+
+            ---
+        <1> --- <2>
+         |      |||
+        <4> --- <3>
+
+        We want to replace 2 with 3:
+
+        <1> --[5 times]
+         |        |
+        <4> ---- <3>
+        """
+        g = Graph({1: [2, 4],
+                   2: [1, 3],
+                   3: [2, 4],
+                   4: [1, 3]})
+
+        g.replace_edge(2, 3)
+        g.sort()
+
+        expected = {1: [3, 3, 4],
+                    3: [1, 1, 4],
+                    4: [1, 3]}
+
+        self.assertEqual(g.graph, expected)
--- week4_graph.py
+++ week4_graph.py
@@ -1,5 +1,7 @@
 from pprint import pprint
 
+from copy import deepcopy
+
 
 def load_input_graph(fixture):
     graph = {}
@@ -19,6 +21,53 @@ class Graph:
 
     def print_stats(self):
         print('Nodes: {}'.format(len(self.graph)))
+
+    def replace_edge(self, a, b):
+        """
+        We remove edge between a and b. We remove a and move all the existing relations into b.
+        :return:
+        """
+        assert a in self.graph[b], "Can't pop edge - reference found <{}>---<{}>".format(a, b)
+        assert b in self.graph[a], "Can't pop edge - reference found <{}>---<{}>".format(b, a)
+
+        # Remove edge <a>---<b>
+        self.remove_from_node(a, b)
+        self.remove_from_node(b, a)
+
+        # Remove node from graph
+        refs = self.graph.pop(a)
+
+        # Add remaining references from a into b
+        self.graph[b].extend(refs)
+        self.graph[b].extend(refs)
+
+        # Replace all the existing "a" references by "b"
+        for nodes in self.graph.values():
+            self.merge_nodes(nodes, a, b)
+
+    def remove_from_node(self, node, value):
+        self.graph[node] = [x for x in self.graph[node] if x != value]
+
+    def merge_nodes(self, nodes, a, b):
+        double_edge = 0
+        for i, value in enumerate(nodes):
+            if value == a:
+                nodes[i] = b
+                double_edge += 1
+
+        # double edge
+        for i in range(double_edge):
+            nodes.append(b)
+
+    def clone(self):
+        return Graph(deepcopy(self.graph))
+
+    def sort(self):
+        """
+        Sort nodes - useful for tests purposes
+        """
+        for nodes in self.graph.values():
+            nodes.sort()
 
 
 if __name__ == '__main__':
